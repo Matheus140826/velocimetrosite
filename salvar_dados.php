3469ac62294377465808671f9cdd0dfcc7d8b146
Matheus140826 <matheus.somera@aluno.senai.br>
2026-06-09 17:46:24 -0300

    Update salvar_dados.php

--- salvar_dados.php
+++ salvar_dados.php
@@ -9,18 +9,16 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-if (isset($_POST['velocidade']) && isset($_POST['rpm'])) {
-    $velocidade = htmlspecialchars($_POST['velocidade'], ENT_QUOTES, 'UTF-8');
-    $rpm = htmlspecialchars($_POST['rpm'], ENT_QUOTES, 'UTF-8');
-   
-    echo "Dados recebidos com sucesso! Velocidade: " . $velocidade . " km/h | RPM: " . $rpm;
-   
-    $stmt = $conn->prepare("INSERT INTO suas_leituras (velocidade, rpm) VALUES (?, ?)");
-    $stmt->bind_param("dd", $velocidade, $rpm);
-    $stmt->execute();
-    $stmt->close();
+$conn = new mysqli("localhost", "root", "", "trabalho");
 
-} else {
+include('buscar_ultimo.php');
+$id = $_POST["id"];
+$nome = $_POST["nome"];
+$sql = "insert into user_teste (id, nome) values ('$id', '$nome')"; 
+mysqli_query($conn, $sql);
+mysqli_close($conn);
+
+{
    
     echo "Erro: Dados de 'velocidade' ou 'rpm' não foram enviados.";
 }
