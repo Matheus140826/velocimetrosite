c0a916894b6e257d74ee4efe47fdac0a45bff5dd
Matheus140826 <matheus.somera@aluno.senai.br>
2026-06-10 17:39:29 -0300

    Update salvar_dados.php

--- salvar_dados.php
+++ salvar_dados.php
@@ -3,25 +3,21 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "trabalho";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
-$conn = new mysqli("localhost", "root", "", "trabalho");
 
-include('buscar_ultimo.php');
-$id = $_POST["id"];
-$nome = $_POST["nome"];
-$sql = "insert into user_teste (id, nome) values ('$id', '$nome')"; 
-mysqli_query($conn, $sql);
+    $velocidade = $_POST["velocidade"];
+    $rpm = $_POST["rpm"];
+    
+    $sql = "INSERT INTO telemetria (velocidade, rpm) VALUES ('$velocidade', '$rpm')";
+    
+    if (mysqli_query($conn, $sql)) {
+        echo "Dados salvos com sucesso!";
+    } else {
+        echo "Erro ao salvar no banco: " . mysqli_error($conn);
+    }
+
 mysqli_close($conn);
-
-{
-   
-    echo "Erro: Dados de 'velocidade' ou 'rpm' não foram enviados.";
-}
-
-$conn->close();
 ?>
